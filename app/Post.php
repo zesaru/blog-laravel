c9daf56c766400e3e761d7594ff909142df2408d
Cesar Murillo <namiayumis@gmail.com>
2020-05-02 22:16:03 +0900

    install sluggable

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'update' => 'true'
+            ]
+        ];
+    }
 }
