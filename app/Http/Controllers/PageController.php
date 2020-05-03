6213cdf1f9a2d28a99652c908a484bb1f924ba09
Cesar Murillo <namiayumis@gmail.com>
2020-05-03 11:18:16 +0900

    create relationuserspost & template post posts

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function posts()
+    {
+        return view('posts', [
+            'post' => Post::with('user')->latest()->paginate()
+        ]);
+    }
+
+
+    public function post(Post $post)
+    {
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
--- app/Post.php
+++ app/Post.php
@@ -26,4 +26,9 @@ class Post extends Model
             ]
         ];
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
